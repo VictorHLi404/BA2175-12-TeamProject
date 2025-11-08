0d00a571b5c78c59c8cef992df381dbed47a010f
hadeel <hadeel30492@yahoo.com>
2025-11-08 17:43:58 -0500

    .

--- src/main/java/data_access/TriviaDataAccess.java
+++ src/main/java/data_access/TriviaDataAccess.java
@@ -17,3 +17,4 @@ public interface TriviaDataAccess {
             throws IOException, InterruptedException;
 }
 
+
--- src/test/java/TriviaFileReaderTest.java
+++ src/test/java/TriviaFileReaderTest.java
@@ -35,5 +35,6 @@ public class TriviaFileReaderTest {
             e.printStackTrace();
         }
     }
+
 }
 
