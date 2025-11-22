a40a74a828686c38149579f875a1ea1da8b28652
Ya-Ching <airatang0150@gmail.com>
2025-11-22 18:19:16 -0500

    ViewScoreInputBoundary added

---
+++ src/main/java/use_case/view_score/ViewScoreInputBoundary.java
@@ -0,0 +1,16 @@
+package use_case.view_score;
+
+
+/**
+ * Input Boundary for actions which are related to View Score.
+ */
+
+public interface ViewScoreInputBoundary {
+    /**
+     * Executes the signup use case.
+     *
+     * @param viewScoreInputData the input data
+     */
+    void execute(ViewScoreInputData viewScoreInputData);
+
+}
