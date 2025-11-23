7f527a7b54dcae4b38d088794059943870d88e49
Ya-Ching <airatang0150@gmail.com>
2025-11-23 00:15:12 -0500

    ViewScoreViewModel added

---
+++ src/main/java/interface_adapter/view_score/ViewScoreViewModel.java
@@ -0,0 +1,13 @@
+package interface_adapter.view_score;
+
+import interface_adapter.ViewModel;
+
+public class ViewScoreViewModel extends ViewModel {
+
+    public static final String SELECT_USER_LABEL = "Select user";
+    public static final String VIEW_SCORE_LABEL = "View Score";
+
+    public ViewScoreViewModel() {
+        super("view Score");
+    }
+}
