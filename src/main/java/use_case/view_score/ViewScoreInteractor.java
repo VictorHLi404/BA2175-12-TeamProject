9ab4f157bec6422aa6b59ff8c78e02beb1b3594f
Ya-Ching <airatang0150@gmail.com>
2025-11-26 15:51:12 -0500

    added sorted QuizResult

--- src/main/java/use_case/view_score/ViewScoreInteractor.java
+++ src/main/java/use_case/view_score/ViewScoreInteractor.java
@@ -4,10 +4,8 @@ import entities.QuizResults;
 import entities.User;
 import persistence.FileReaderGateway;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 
 /**
@@ -40,22 +38,25 @@ public class ViewScoreInteractor implements ViewScoreInputBoundary
 
         UUID userID = user.getUserId();
         Map<UUID, QuizResults> allQuizResults = userDataReadObject.loadAllQuizResults();
-        List<UUID> userQuizResult = new ArrayList<>();
+        List<PerQuizResultData> perQuizData = new ArrayList<>();
         int totalQuestions = 0;
         int totalCorrectAnswers = 0;
         for (QuizResults quizResults : allQuizResults.values()){
             if (quizResults.getUserId().equals(userID)){
                 totalQuestions += quizResults.getQuizSize();
                 totalCorrectAnswers += quizResults.getScore();
+                String timeStamp = quizResults.getTimestamp().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+                perQuizData.add(new PerQuizResultData(timeStamp, quizResults.getScore(), quizResults.getQuizSize()));
             }
         }
-        if (totalQuestions == 0){
+        if (perQuizData.isEmpty()){
             viewScoreOutputBoundary.prepareNoResultsView(username);
         }
         else {
+            perQuizData.sort(Comparator.comparing(PerQuizResultData::getDateTime));
             double rawScore = (double) totalCorrectAnswers / totalQuestions;
             int scorePercentage = (int) (rawScore * 100);
-            ViewScoreOutputData outputData = new ViewScoreOutputData(username, scorePercentage);
+            ViewScoreOutputData outputData = new ViewScoreOutputData(username, scorePercentage,perQuizData);
             viewScoreOutputBoundary.prepareSuccessView(outputData);
         }
 
